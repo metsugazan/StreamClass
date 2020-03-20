53d2f83f51991832941d38b684833aae3cb557db
Sckald <damienchereault@gmail.com>
2020-03-20 11:29:43 +0100

    InitialPush

--- StreamClass/Controllers/HomeController.cs
+++ StreamClass/Controllers/HomeController.cs
@@ -10,7 +10,7 @@ namespace StreamClass.Controllers
 {
     public class HomeController : Controller
     {
-        public IActionResult Index()
+        public IActionResult Index()//test
         {
             return View();
         }
